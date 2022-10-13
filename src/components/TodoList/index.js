69c8ea11ef617eb818c209e4b12f0516e9e7b8e1
duong56 <91894323+duong56@users.noreply.github.com>
2022-10-13 20:39:35 +0700

    Install module

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,7 +1,17 @@
 import { Col, Row, Input, Button, Select, Tag } from 'antd';
+import useBearStore from '../../zustand/store';
 import Todo from '../Todo';
 
 export default function TodoList() {
+
+  const todoList = useBearStore((state)=> state.todoList)
+
+  console.log(todoList)
+
+  const handleInputChange = (e)=>{
+    console.log(e.target.value)
+  }
+
   return (
     <Row style={{ height: 'calc(100% - 40px)' }}>
       <Col span={24} style={{ height: 'calc(100% - 40px)', overflowY: 'auto' }}>
@@ -11,7 +21,7 @@ export default function TodoList() {
       </Col>
       <Col span={24}>
         <Input.Group style={{ display: 'flex' }} compact>
-          <Input />
+          <Input onChange={handleInputChange}/>
           <Select defaultValue="Medium">
             <Select.Option value='High' label='High'>
               <Tag color='red'>High</Tag>
